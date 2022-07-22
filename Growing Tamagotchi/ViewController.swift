a82919ab138774e77facd4b9703e3719c4240c8b
jiun <vonreal20@gmail.com>
2022-07-22 15:27:36 +0900

    Initial Commit

---
+++ Growing Tamagotchi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Growing Tamagotchi
+//
+//  Created by 나지운 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
